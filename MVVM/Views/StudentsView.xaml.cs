ea4ad5072d785cbf81fe39a04cd375f6bdef2bf4
Dominik <dominik.zemlik.dev@gmail.com>
2023-06-03 13:33:53 +0200

    Add skeleton view for add student modal

--- MVVM/Views/StudentsView.xaml.cs
+++ MVVM/Views/StudentsView.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace DriveBuddyWpfApp.MVVM.Views
 {
-    /// <summary>
-    /// Interaction logic for StudentsView.xaml
-    /// </summary>
     public partial class StudentsView : UserControl
     {
         public StudentsView()
         {
             InitializeComponent();
+        }
+
+        private void OpenCreateStudentModal_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var createStudentModal = new CreateStudentModalView();
+            createStudentModal.ShowDialog();
         }
     }
 }
