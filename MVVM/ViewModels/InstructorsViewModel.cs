0b10faf0fe592eec046a8ed427bf5044c6f6fc0d
Dominik <dominik.zemlik.dev@gmail.com>
2023-06-06 16:35:16 +0200

    Add logic for delete instructor

--- MVVM/ViewModels/InstructorsViewModel.cs
+++ MVVM/ViewModels/InstructorsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using DriveBuddyWpfApp.Core;
 using DriveBuddyWpfApp.MVVM.Models;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DriveBuddyWpfApp.MVVM.ViewModels
 {
@@ -20,11 +22,39 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             }
         }
 
+        #region ===== Commands =====
+
+        public ICommand DeleteInstructorCommand { get; set; }
+
+        #endregion
+
         public InstructorsViewModel()
         {
             _db = new DriveBuddyEntities();
             LoadInstructors();
+
+            DeleteInstructorCommand = new RelayCommand(DeleteInstructor);   
         }
+
+        #region ===== Action Methods =====
+
+        private void DeleteInstructor(object obj)
+        {
+            try
+            {
+                var instructor = obj as Instructor;
+                _db.Instructors.Remove(instructor);
+                _db.SaveChanges();
+                InstructorsList.Remove(instructor);
+                MessageBox.Show("Instructor has been deleted", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong. Instructor has not been deleted.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
 
         private void LoadInstructors() => InstructorsList = new ObservableCollection<Instructor>(_db.Instructors);
     }
