679d6a6a627cf54c34dfe05feb10ec28d5342c0a
Dominik <dominik.zemlik.dev@gmail.com>
2023-06-06 16:55:09 +0200

    Add logic for delete lesson

--- MVVM/ViewModels/LessonsViewModel.cs
+++ MVVM/ViewModels/LessonsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using DriveBuddyWpfApp.Core;
 using DriveBuddyWpfApp.MVVM.Models;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DriveBuddyWpfApp.MVVM.ViewModels
 {
@@ -20,12 +22,40 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             }
         }
 
+        #region ===== Commands =====
+
+        public ICommand DeleteLessonCommand { get; set; }
+
+        #endregion
+
         public LessonsViewModel()
         {
             _db = new DriveBuddyEntities();
             LoadLessons();
+
+            DeleteLessonCommand = new RelayCommand(DeleteLesson);
         }
 
         private void LoadLessons() => LessonsList = new ObservableCollection<Lesson>(_db.Lessons);
+
+        #region ===== Action Methods =====
+
+        private void DeleteLesson(object obj)
+        {
+            try
+            {
+                var lesson = obj as Lesson;
+                _db.Lessons.Remove(lesson);
+                _db.SaveChanges();
+                LessonsList.Remove(lesson);
+                MessageBox.Show("Lesson has been deleted", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong. Lesson has not been deleted.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
     }
 }
