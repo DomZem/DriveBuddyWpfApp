e6afca95615ecdc9b20e8ab0f755d7d1ba4e25d6
Dominik <dominik.zemlik.dev@gmail.com>
2023-06-26 09:18:10 +0200

    Add XML documentation for cars view model

--- MVVM/ViewModels/CarsViewModel.cs
+++ MVVM/ViewModels/CarsViewModel.cs
@@ -9,40 +9,70 @@ using System.Windows.Input;
 
 namespace DriveBuddyWpfApp.MVVM.ViewModels
 {
+    /// <summary>
+    /// Represents the view model for managing cars.
+    /// </summary>
     public class CarsViewModel : ObservableObject
     {
-        DriveBuddyEntities _db;
+        private DriveBuddyEntities _db;
 
+        /// <summary>
+        /// Get the list of cars from database.
+        /// </summary>
         public ObservableCollection<Car> CarsList { get; set; } = new ObservableCollection<Car>();
 
+        /// <summary>
+        /// Get the list of categories from database.
+        /// </summary>
         public ObservableCollection<Category> CategoriesList { get; set; } = new ObservableCollection<Category>();
 
+        /// <summary>
+        /// Get the selected car to update.
+        /// </summary>
         public Car SelectedCar { get; set; } = new Car();
 
         private Car _newCar = new Car();
 
+        /// <summary>
+        /// Get the new car.
+        /// </summary>
         public Car NewCar
         {
             get => _newCar;
             set
             {
-                _newCar = value;    
+                _newCar = value;
                 OnPropertyChanged(nameof(NewCar));
             }
         }
 
         #region ===== Commands =====
 
+        /// <summary>
+        /// Command for deleting a car in database.
+        /// </summary>
         public ICommand DeleteCarCommand { get; set; }
 
+        /// <summary>
+        /// Command for adding a new car in database.
+        /// </summary>
         public ICommand AddCarCommand { get; set; }
 
+        /// <summary>
+        /// Command for setting the selected car to update.
+        /// </summary>
         public ICommand SetSelectedCarCommand { get; set; }
 
+        /// <summary>
+        /// Command for updating a car in datbase.
+        /// </summary>
         public ICommand UpdateCarCommand { get; set; }
 
         #endregion
 
+        /// <summary>
+        /// Initialize a new instance of the <see cref="CarsViewModel"/> class.
+        /// </summary>
         public CarsViewModel()
         {
             _db = new DriveBuddyEntities();
@@ -53,11 +83,15 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             SetSelectedCarCommand = new RelayCommand(SetSelectedCar);
             UpdateCarCommand = new RelayCommand(UpdateCar);
 
-            NewCar.ReviewDate = DateTime.Now.AddDays(1);    
+            NewCar.ReviewDate = DateTime.Now.AddDays(1);
         }
 
         #region ===== Action Methods =====
 
+        /// <summary>
+        /// Delete a car and display a message about the performed operation.
+        /// </summary>
+        /// <param name="obj">The car object to delete.</param>
         private void DeleteCar(object obj)
         {
             try
@@ -74,6 +108,10 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             }
         }
 
+        /// <summary>
+        /// Add a new car and display a message about the performed operation.
+        /// </summary>
+        /// <param name="obj">The object representing the new car.</param>
         private void AddCar(object obj)
         {
             try
@@ -99,6 +137,10 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             }
         }
 
+        /// <summary>
+        /// Set the selected car to update and open the update car modal view.
+        /// </summary>
+        /// <param name="obj">The selected car object.</param>
         private void SetSelectedCar(object obj)
         {
             var car = obj as Car;
@@ -108,12 +150,16 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
             updateCarModalView.ShowDialog();
         }
 
+        /// <summary>
+        /// Update the selected car and display a message about the performed operation
+        /// </summary>
+        /// <param name="obj">The selected car object.</param>
         private void UpdateCar(object obj)
         {
             try
             {
                 var car = _db.Cars.Find(SelectedCar.CarID);
-                _db.Entry(car).CurrentValues.SetValues(SelectedCar); 
+                _db.Entry(car).CurrentValues.SetValues(SelectedCar);
                 _db.SaveChanges();
                 MessageBox.Show($"Car has been updated", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
             }
@@ -125,10 +171,13 @@ namespace DriveBuddyWpfApp.MVVM.ViewModels
 
         #endregion
 
-        private void LoadData() 
-        { 
+        /// <summary>
+        /// Load the initial data for cars and categories.
+        /// </summary>
+        private void LoadData()
+        {
             CarsList = new ObservableCollection<Car>(_db.Cars);
             CategoriesList = new ObservableCollection<Category>(_db.Categories);
-        } 
+        }
     }
 }
